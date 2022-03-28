7edb842e448f517b884d0886ba8f57591f83ef88
Tashmeet1996 <95742557+Tashmeet1996@users.noreply.github.com>
2022-03-28 00:22:29 -0700

    Add files via upload

---
+++ employeeModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/empDB", {
+  useNewUrlParser: true,
+});
+
+const emp = mongoose.Schema({
+    empID: { type: Number, required: true, unique: true }, 
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  DOB: { type: Date, required: true },
+  address: { type: String, required: true },
+  SSN: { type: Number, required: true },
+});
+
+const empItem = mongoose.model("Employee Item", emp);
+ 
+  module.exports = empItem;
+  
